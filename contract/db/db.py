9a4c2b19528d8118e141a3fae89c697a60108f5a
Pasha Mazurov <pashamazurov@gmail.com>
2026-08-29 13:15:12 +0200

    Add homework 30 allure

--- contract/db/db.py
+++ contract/db/db.py
@@ -6,7 +6,8 @@ from sqlalchemy.orm import sessionmaker
 
 # З'єднання з базою даних PostgreSQL
 # Потрібно вказати правильні дані для вашої бази даних
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1986@postgres-db:5432/students_db")
+#DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1986@postgres-db:5432/students_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1986@localhost:5432/students_db")
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()   #клас до якого ми наслудіємся, має лежати коремо
 
--- tests/db_tests/test_orm_homework_21.py
+++ tests/db_tests/test_orm_homework_21.py
@@ -7,6 +7,7 @@ from sqlalchemy.sql.expression import select, update
 from contract.db.db import session
 from contract.db.orm_courses_table import OrmCourses
 from contract.db.orm_student_table import OrmStudent
+import allure
 
 f = Faker()
 
@@ -24,79 +25,110 @@ def random_course_id():
     random_course_id = random.choice(res)
     return random_course_id
 
-"""Виконання базових операцій: Напишіть програму, 
-яка додає нового студента до бази даних та додає його до певного курсу. 
-Переконайтеся, що ці зміни коректно відображаються у базі даних."""
-@pytest.mark.add    #марка для запуску тесту
-def test_add_student_course_orm(random_course_id):
-    # Додавання нового студента
-    new_student = OrmStudent(name=f.name(), age=random.choice(range(16, 60))) #створюєм 1  студентів з рандомним віком
-    session.add(new_student)   #додаєм студента .add
-    session.commit()
-    # отримання інформації про потрібний курс
 
-    course_query = select(OrmCourses).where(OrmCourses.id == random_course_id)  #запит який отримує курс
-    result = session.scalars(course_query).first()    # виконуєм запит/.first() бо очікуємо ОДИН результат
+@allure.epic('Студенти')
+@allure.feature('Студенти та курси (ORM) - додавання/видалення, отримання інформації про курс/студента')
+@allure.link(url='jira-board/ed-01', name='ed-01')
+@pytest.mark.students_crud
+class TestStudents:
+    """Виконання базових операцій: Напишіть програму,
+    яка додає нового студента до бази даних та додає його до певного курсу.
+    Переконайтеся, що ці зміни коректно відображаються у базі даних."""
+    @allure.severity(allure.severity_level.NORMAL)
+    @allure.description('Цей тест додає студента на курс і перевіряє чи додавання відбулось')
+    @allure.story('Реєстрація на курс')
+    @allure.title('Додавання студента до курсу')
+    @pytest.mark.add  # марка для запуску тесту
+    def test_add_student_course_orm(self,random_course_id):
 
-    new_student.courses.append(result)# записуємо студента на курс: додаємо курс у список student.courses
-    session.commit()
+        with allure.step('Додати нового студента'):
+            new_student = OrmStudent(name=f.name(), age=random.choice(range(16, 60)))
+            session.add(new_student)
+            session.commit()
 
-    upd_student = session.scalars(select(OrmStudent).where(OrmStudent.id == new_student.id)).first() # повторно робимо запит до БД і дістаємо цього ж студента за його id
-    assert result in upd_student.courses    # перевіряємо: чи є доданий курс (result) у списку курсів студента після перечитування з БД
-    print(upd_student, upd_student.courses)
+        with allure.step(f'Отримати курс за id {random_course_id} і записати студента на нього'):
+            course_query = select(OrmCourses).where(OrmCourses.id == random_course_id)
+            result = session.scalars(course_query).first()
+            new_student.courses.append(result)
+            session.commit()
 
-"""Запити до бази даних: Напишіть запити до бази даних, 
-які повертають інформацію про студентів, зареєстрованих на певний курс, 
-або курси, на які зареєстрований певний студент."""
+        with allure.step('Перевірити, що курс з’явився у списку курсів студента'):
+            upd_student = session.scalars(select(OrmStudent).where(OrmStudent.id == new_student.id)).first()
+            assert result in upd_student.courses
+            print(upd_student, upd_student.courses)
 
-@pytest.mark.info   #марка для запуску тесту
-def test_info_students_on_course(random_course_id):
+    """Запити до бази даних: Напишіть запити до бази даних,
+    які повертають інформацію про студентів, зареєстрованих на певний курс,
+    або курси, на які зареєстрований певний студент."""
 
-    info_students_query = select(OrmCourses).where(OrmCourses.id == random_course_id)
-    result = session.scalars(info_students_query).first()
-    print(result.students)
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.description('Цей тест отримує інформацію про студента і його курс')
+    @allure.story('Перегляд інформації')
+    @allure.title('Отримання інформації про студента, зареєстрованих на певний курс')
+    @pytest.mark.info  # марка для запуску тесту
+    def test_info_students_on_course(self, random_course_id):
+        with allure.step(f'Отримати курс за id {random_course_id} та список студентів на ньому'):
+            info_students_query = select(OrmCourses).where(OrmCourses.id == random_course_id)
+            result = session.scalars(info_students_query).first()
+            print(result.students)
 
-@pytest.mark.info
-def test_info_courses_of_students(random_student_id):
-    info_course_query = select(OrmStudent).where(OrmStudent.id == random_student_id)    #умови що вивести
-    result = session.scalars(info_course_query).first() #виконуєм запит
-    print(result.courses)
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.description('Цей тест отримує інформацію про курс і студентов на ньому')
+    @allure.story('Перегляд інформації')
+    @allure.title('Отримання інформації про курси на які зареєстрований певний студент')
+    @pytest.mark.info # марка для запуску тесту
+    def test_info_courses_of_students(self,random_student_id):
+        with allure.step(f'Отримати курси за id {random_student_id}'):
+            info_course_query = select(OrmStudent).where(OrmStudent.id == random_student_id)    #умови що вивести
+            result = session.scalars(info_course_query).first() #виконуєм запит
+            print(result.courses)
 
-"""Оновлення та видалення даних: Реалізуйте можливість оновлення даних про студентів або курси,
- а також видалення студентів з бази даних."""
-@pytest.mark.update
-def test_upd_student(random_student_id):
+    """Оновлення та видалення даних: Реалізуйте можливість оновлення даних про студентів або курси,
+     а також видалення студентів з бази даних."""
+    @allure.severity(allure.severity_level.TRIVIAL)
+    @allure.description('Цей тест оновлює інформацію про студента і перевіряє її')
+    @allure.story('Зміна даних студента')
+    @allure.title('Оновлення данних про студента')
+    @pytest.mark.update
+    def test_upd_student(self,random_student_id):
+        with allure.step('Отримуєм данні про студента'):
+            student_query = select(OrmStudent).where(OrmStudent.id == random_student_id)   # запит щоб дістати старі значення для очік результату
+            exp_student_res = session.scalars(student_query).first()
 
-    student_query = select(OrmStudent).where(OrmStudent.id == random_student_id)   # запит щоб дістати старі значення для очік результату
-    exp_student_res = session.scalars(student_query).first()
+            name_student = exp_student_res.name     #очік ім'я
+            age = exp_student_res.age               #очік вік
 
-    name_student = exp_student_res.name     #очік ім'я
-    age = exp_student_res.age               #очік вік
+            exp_name_student = f.first_name() # нові значення
+            exp_age_student = random.choice(range(20, 40))
 
-    exp_name_student = f.first_name() # нові значення
-    exp_age_student = random.choice(range(20, 40))
+        with allure.step('Змінюєм данні'):
+            student_upd_query = (update(OrmStudent).where(OrmStudent.id == random_student_id)       # запит на оновлення студента за id
+                                  .values(name=exp_name_student, age=exp_age_student))
+            session.execute(student_upd_query)  #виконуєм запит
+            session.commit()    #комітим зіміни
 
+        with allure.step('Перевіряєм змінені данні'):
+            ar_student_res = session.scalars(select(OrmStudent).where(OrmStudent.id == random_student_id)).first()  # перечитуємо студента з бази, щоб перевірити, що зміни реально збереглись
+            assert ar_student_res.name != name_student
+            #assert ar_student_res.age != age
+            assert ar_student_res.age == age #тест завален спеціально для виводу в алюр
+            assert ar_student_res.name == exp_name_student
+            assert ar_student_res.age == exp_age_student
+            assert ar_student_res.id == random_student_id
 
-    student_upd_query = (update(OrmStudent).where(OrmStudent.id == random_student_id)       # запит на оновлення студента за id
-                          .values(name=exp_name_student, age=exp_age_student))
-    session.execute(student_upd_query)  #виконуєм запит
-    session.commit()    #комітим зіміни
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.description('Цей тест видаляє студента і перевіряє чи видалення відбулось')
+    @allure.story('Зміна даних студента')
+    @allure.title('Видалення студента')
+    @pytest.mark.delete
+    def test_del_student(self, random_student_id):
+        with allure.step(f'Отримати та видалити студента з id: {random_student_id}'):
+            student_to_del_query = select(OrmStudent).where(OrmStudent.id == random_student_id)
+            student = session.scalars(student_to_del_query).first()
+            session.delete(student)
+            session.commit()
 
-    ar_student_res = session.scalars(select(OrmStudent).where(OrmStudent.id == random_student_id)).first()     # перечитуємо студента з бази, щоб перевірити, що зміни реально збереглись
-
-    assert ar_student_res.name != name_student
-    assert ar_student_res.age != age
-    assert ar_student_res.name == exp_name_student
-    assert ar_student_res.age == exp_age_student
-    assert ar_student_res.id == random_student_id
-@pytest.mark.delete
-def test_del_student(random_student_id):
-    student_to_del_query = select(OrmStudent).where(OrmStudent.id == random_student_id)
-    student = session.scalars(student_to_del_query).first()   # виконуємо запит і отримуємо сам об'єкт
-
-    session.delete(student)
-    session.commit()
-
-    check_query = select(OrmStudent).where(OrmStudent.id == random_student_id)
-    check_result = session.scalars(check_query).first()
-    assert check_result is None
+        with allure.step('Перевірка видалення студента'):
+            check_query = select(OrmStudent).where(OrmStudent.id == random_student_id)
+            check_result = session.scalars(check_query).first()
+            assert check_result is None
